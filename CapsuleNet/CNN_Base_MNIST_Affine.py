softmax bug fix

CapsuleNet/CNN_Base_MNIST_Affine.py
@@ -14,33 +14,28 @@ from tensorflow.examples.tutorials.mnist import input_data
 import affNIST
 import util
 
-modelName = './weights/conventionalCNN.pd'
+modelName = './weights/baseCNN.pd'
 
 FREQ = 2
-epoch = 500
-BATCH = 50
-REDUCE_DATA_COUNT_RATIO = 1
+epoch = 2
+BATCH = 200
+REDUCE_DATA_COUNT_RATIO = 100
 learning_rate = 1e-3
-isNewTrain =  not True     
+isNewTrain =  True     
 
-def conventionalCNN(x):
+def baseCNN(x,isTrain=False):
     
-    with slim.arg_scope([slim.conv2d], normalizer_fn=slim.batch_norm, padding='SAME', weights_initializer=tf.contrib.layers.xavier_initializer(),weights_regularizer=slim.l2_regularizer(0.00001)):
-        pool = slim.conv2d(x, 32,[3,3],[1,1])
-        pool = slim.max_pool2d(pool,[2,2],[2,2])#14x14
+    with slim.arg_scope([slim.conv2d], normalizer_fn=None, padding='VALID', weights_initializer=tf.contrib.layers.xavier_initializer(),weights_regularizer=slim.l2_regularizer(0.00001)):
+        pool = slim.conv2d(x, 255,[5,5],[1,1])        
+        pool = slim.conv2d(pool, 255,[5,5],[1,1])        
+        pool = slim.conv2d(pool, 128,[5,5],[1,1])#(?,16,16,128)
         print ('    pool',pool)  
-
-        pool = slim.conv2d(pool, 64,[3,3],[1,1])
-        pool = slim.max_pool2d(pool,[2,2],[2,2])#7x7
-        print ('    pool',pool)  
-
-        pool = slim.conv2d(pool, 128,[3,3],[1,1])
-        pool = slim.max_pool2d(pool,[2,2],[2,2])#3x3
-        print ('    pool',pool)  
-
-        pool = slim.conv2d(pool, 10,[3,3],[1,1], padding='VALID')
-        
-        out = tf.squeeze(pool,[1,2])
+        fc = slim.flatten(pool) #(?,32768)
+        print ('    fc',fc)  
+        fc  = slim.fully_connected(fc, 328)
+        fc  = slim.fully_connected(fc, 192)
+        fc  = slim.dropout(fc, 0.8, is_training = isTrain )
+        out = slim.fully_connected(fc, 10)
         print ('    out',out)        
         
     return out
@@ -57,15 +52,17 @@ def main(arg=None):
 
     X = tf.placeholder(tf.float32, [None, None,None,1])
     Y = tf.placeholder(tf.float32, [None])
+    D = tf.placeholder(tf.bool)
     y_int = tf.cast(Y, tf.int32)
     Y_ONE_HOT = tf.one_hot(y_int,10)
 
     x_4d = tf.image.resize_bilinear(X, [28, 28])
-    hyperthesis = conventionalCNN(x_4d)    
+    hyperthesis = baseCNN(x_4d,D)    
     print ('    hyperthesis',hyperthesis)     
         
     loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y_int, logits=hyperthesis))        
     train_step = tf.train.AdamOptimizer(learning_rate, 0.9).minimize(loss)
+    #train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
 
     predict = tf.cast(tf.argmax(hyperthesis, 1),tf.int32)
     accuracy = tf.reduce_mean(tf.cast(tf.equal(predict, y_int), tf.float32))
@@ -92,7 +89,7 @@ def main(arg=None):
                 batch_x = x[start:end]
                 if Pad: batch_x = util.padding(batch_x)
                 else: batch_x = np.reshape(batch_x, [-1,h,w,1])
-                feed = {X:batch_x , Y: y[start:end]}    
+                feed = {X:batch_x , Y: y[start:end],D:train}    
                 if train: _,ML,acc = sess.run([train_step,loss,accuracy],feed)            
                 else : ML,acc = sess.run([loss,accuracy],feed)
                 acc_sum += acc/iter

CapsuleNet/CNN_Base_MNIST_Overlap.py
@@ -11,17 +11,16 @@ import CapsuleLayer
 
 modelName = './weights/base_cnn_overlap.pd'
 AFFIN = True
-RECONSTRUCT = True
 FREQ = 10
 epoch = 1000
-BATCH = 600#must even number
+BATCH = 100#must even number
 REDUCE_DATA_COUNT_RATIO = 1
-learning_rate = 1e-4
-isNewTrain =  True     
+learning_rate = 1e-7
+isNewTrain = not True     
  
 def baseCNN(x,isTrain=False):
     
-    with slim.arg_scope([slim.conv2d], normalizer_fn=None, padding='VALID', weights_initializer=tf.contrib.layers.xavier_initializer(),weights_regularizer=slim.l2_regularizer(0.00001)):
+    with slim.arg_scope([slim.conv2d], normalizer_fn=slim.batch_norm, padding='VALID', weights_initializer=tf.contrib.layers.xavier_initializer(),weights_regularizer=slim.l2_regularizer(0.00001)):
         pool = slim.conv2d(x, 255,[5,5],[1,1])        
         pool = slim.conv2d(pool, 255,[5,5],[1,1])        
         pool = slim.conv2d(pool, 128,[5,5],[1,1])#(?,16,16,128)
@@ -69,7 +68,11 @@ def main(arg=None):
     hyperthesis = baseCNN(x_overlap,D)
         
     loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_overlap, logits=hyperthesis))                
-    train_step = tf.train.AdamOptimizer(learning_rate).minimize(loss)
+    optimizer = tf.train.AdamOptimizer(learning_rate)    
+    gvs = optimizer.compute_gradients(loss)
+    capped_gvs = [(tf.clip_by_value(grad, -1., 1.), var) for grad, var in gvs]
+    train_step = optimizer.apply_gradients(capped_gvs)
+
     top_values, top_predict = tf.nn.top_k(hyperthesis,2)
     
     y_gt = tf.stack([y_int[0::2],y_int[1::2]], 1)

CapsuleNet/CapsuleLayer.py
@@ -56,7 +56,7 @@ def capsNetBasic(x, reuse = False):
 def capsnet_forward(x, reuse = False):
     with tf.variable_scope('CapsNet',reuse=reuse):
         wcap = tf.get_variable('wcap',[1,6*6*32,8,10,16],initializer=tf.truncated_normal_initializer(stddev=0.02))
-        b = tf.get_variable('coupling_coefficient_logits',[1,6*6*32,10,1],initializer=tf.constant_initializer(0.0))
+        b = tf.zeros([1,6*6*32,10,1])
 
     with slim.arg_scope([slim.conv2d], normalizer_fn=slim.batch_norm, padding='VALID', weights_initializer=tf.contrib.layers.xavier_initializer(),weights_regularizer=slim.l2_regularizer(0.00001)):
         
@@ -73,7 +73,7 @@ def capsnet_forward(x, reuse = False):
         print ('    u_',u_)#(10, 1152, 10, 16)
         
         for i in range(ROUT_COUNT):            
-            c = tf.nn.softmax(b, dim=3)
+            c = tf.nn.softmax(b, dim=2)
             c = tf.stop_gradient(c)
             
             print (i,'    c',c)#(10, 1152, 10, 1)

CapsuleNet/Train_MNIST_Affine.py
@@ -14,14 +14,14 @@ import util
 import CapsuleLayer
 
 modelName = './weights/caps.pd'
-AFFIN = True
+AFFINE = True
 RECONSTRUCT = True
 FREQ = 10
 epoch = 500
 BATCH = 200
 REDUCE_DATA_COUNT_RATIO = 100
 learning_rate = 1e-3
-isNewTrain =  True     
+isNewTrain =   True     
 
 def main(arg=None):
     
@@ -32,7 +32,7 @@ def main(arg=None):
     print ('  MNIST min',np.min(mnist.train.images[0]),np.max(mnist.train.images[0]))
 
     h = w = 28
-    if AFFIN: h = w = 40
+    if AFFINE: h = w = 40
 
     X = tf.placeholder(tf.float32, [None, None,None,1])
     Y = tf.placeholder(tf.float32, [None])
@@ -51,6 +51,7 @@ def main(arg=None):
     if RECONSTRUCT: loss += 5e-5 * restruc_loss
     
     train_step = tf.train.AdamOptimizer(learning_rate).minimize(loss)
+    #train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
 
     predict = tf.cast(tf.argmax(hyperthesis, 1),tf.int32)
     accuracy = tf.reduce_mean(tf.cast(tf.equal(predict, y_int), tf.float32))